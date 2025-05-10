073ace2d8a1311b4445a1bac0211c9ae46cc6cb1
melkiurak <melkiurak3209@gmail.com>
2025-05-10 12:12:40 +0300

    Add font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~assets/css/main.css'],
+  vite: {
+    plugins:[
+      tailwindcss(),
+    ],
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/test-utils',
