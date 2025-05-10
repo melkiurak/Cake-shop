0b450ce0e24c02131897cea14729f9d2efbf085b
melkiurak <melkiurak3209@gmail.com>
2025-05-10 11:53:41 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@pinia/nuxt'
+  ]
+})
